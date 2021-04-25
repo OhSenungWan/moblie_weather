70de1e3ca4c3db5a6332d227127afaa5570ea04c
DESKTOP-LTDRE58\82104 <jjin4073@naver.com>
2021-04-25 16:46:41 +0900

    충돌?
    충돌?

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -17,8 +17,13 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
-<<<<<<< HEAD
+import static android.graphics.Color.DKGRAY;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.io.IOException;
+import java.util.Calendar;
 import static android.graphics.Color.DKGRAY;
 
 public class MainActivity extends AppCompatActivity {
@@ -29,28 +34,10 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-=======
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
->>>>>>> 87929b25dd42915b8332a1077747d239d928fd32
-
-import java.io.IOException;
-import java.util.Calendar;
-
-<<<<<<< HEAD
-
         setStart();
     }
 
     public void setStart() {
-
-
-
     }
 }
-=======
-import static android.graphics.Color.DKGRAY;
 
->>>>>>> 87929b25dd42915b8332a1077747d239d928fd32
