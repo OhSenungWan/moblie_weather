86b6142e0c72b1f404850e72080a3a0d5bf8e801
DESKTOP-LTDRE58\82104 <jjin4073@naver.com>
2021-05-02 15:46:53 +0900

    메인 정리

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -34,19 +35,32 @@ public class MainActivity extends AppCompatActivity {
     String point_weather = "11B00000";
     String weather;
     String temp;
+    String pop;
+    String vec;
+    String wsd;
     setdata_air sa;
     set_weather sw;
     TextView Location;
     TextView Comment;
     TextView Temp;
     TextView Days_of_week;
+    ImageView Main_img;
+    TextView Pop;
+    TextView Wsd;
+    TextView Vec;
+    TextView Pm25;
+    TextView Pm10;
+    String pm10;
+    String pm25;
+    TextView Pm10_grade;
+    TextView Pm25_grade;
+    TextView Time_comment;
 
     public void setStart() {
         new Thread(new Runnable() {
             @Override
             public void run() {
                 setdata_short cd = new setdata_short();
-                System.out.println(DOW);
                 sa = new setdata_air();
                 sw = new set_weather();
                 setdata_long_Temp slt = new setdata_long_Temp();
@@ -54,14 +68,28 @@ public class MainActivity extends AppCompatActivity {
                 Short_Data = cd.setdata(x_point,y_point);
                 DOW = cd.DOW;
                 Data_Air = sa.setdata_air(city[0]);
+                pm10 = sa.set_pm10();
+                pm25 = sa.set_pm25();
                 Long_Temp = slt.setdata_longtemp(point_temp);
                 Long_Weather = slw.setdata_longweather(point_weather);
                 weather = sw.set_weather(Short_Data);
                 temp = sw.set_temp(Short_Data);
+                pop = sw.set_pop(Short_Data);
+                vec = sw.set_vec(Short_Data);
+                wsd = sw.set_wsd(Short_Data);
                 Location = (TextView)findViewById(R.id.location);
                 Comment = (TextView)findViewById(R.id.comment);
                 Temp = (TextView)findViewById(R.id.temp);
                 Days_of_week = (TextView)findViewById(R.id.days_of_week);
+                Main_img = (ImageView)findViewById(R.id.main_img);
+                Pop = (TextView)findViewById(R.id.pop);
+                Wsd = (TextView)findViewById(R.id.wsd);
+                Vec = (TextView)findViewById(R.id.vec);
+                Pm25 = (TextView)findViewById(R.id.pm25);
+                Pm10 = (TextView)findViewById(R.id.pm10);
+                Pm10_grade = (TextView)findViewById(R.id.pm10grade);
+                Pm25_grade = (TextView)findViewById(R.id.pm25grade);
+                Time_comment = (TextView)findViewById(R.id.time_comment);
                 MainActivity.this.runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
@@ -69,6 +97,36 @@ public class MainActivity extends AppCompatActivity {
                         Comment.setText(weather);
                         Temp.setText(temp+"℃");
                         Days_of_week.setText(DOW);
+                        switch (weather){
+                            case "rain":
+                                Main_img.setImageResource(R.drawable.rain);
+                                Time_comment.setText("지금은 비가 와요.");
+                                break;
+                            case "snow":
+                                Main_img.setImageResource(R.drawable.snow);
+                                Time_comment.setText("밖은 지금 눈이 내려요.");
+                                break;
+                            case "sunny":
+                                Main_img.setImageResource(R.drawable.sun);
+                                Time_comment.setText("해가 뜨는 화창한 날씨에요.");
+                                break;
+                            case "cloud":
+                                Main_img.setImageResource(R.drawable.cloud1);
+                                Time_comment.setText("구름이 많은 날씨에요.");
+                                break;
+                            case "blur":
+                                Main_img.setImageResource(R.drawable.cloud2);
+                                Time_comment.setText("밖은 지금 매우 흐려요.");
+                                break;
+                        }
+                        Pop.setText(pop+"%");
+                        Wsd.setText(wsd+"m/s");
+                        Vec.setText(vec);
+                        Pm10.setText(Data_Air[0]+"㎍/m3");
+                        Pm25.setText(Data_Air[1]+"㎍/m3");
+                        Pm10_grade.setText(pm10);
+                        Pm25_grade.setText(pm25);
+
                     }
                 });
             }
